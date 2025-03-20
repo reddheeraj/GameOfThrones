b20fceb2ba4b3a214d11d3c3a183934eca91b25f
reddheeraj <meetdheerajreddy@gmail.com>
2025-03-19 22:56:18 -0500

    changes

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
+
 LOG_DIR = "logs"
 CHROMA_DB_PATH = "chromadb"
 SQLITE_DB_PATH = "sqlite"
 PROJECT_DIR = os.getcwd()
 CONNECTION_NAME = "GOT"
+
+MODEL = 'llama3.1'
--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import ollama
-
-MODEL = 'llama3.1'
+from config import MODEL
 
 def request_ollama(prompt):
     response = ollama.chat(model=MODEL, messages=[{"role": "user", "content": prompt}])
